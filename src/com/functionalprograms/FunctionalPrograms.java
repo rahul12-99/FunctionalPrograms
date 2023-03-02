1a42c39674f2fb858f2c82d34879040e428b81af
rahul12-99/rahul12-99 <rahulkrrao162@gmail.com>
2023-03-03 04:37:00 +0530

    Added: Welcome messgae

---
+++ src/com/functionalprograms/FunctionalPrograms.java
@@ -0,0 +1,10 @@
+package com.functionalprograms;
+
+import java.util.Scanner;
+
+public class FunctionalPrograms {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to functional programs");
+    }
+}
