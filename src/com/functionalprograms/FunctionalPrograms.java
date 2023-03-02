425eabc008c596c4996a14b01ab922472b7109ee
rahul12-99/rahul12-99 <rahulkrrao162@gmail.com>
2023-03-03 04:40:30 +0530

    Added: Code for two dimensional array

--- src/com/functionalprograms/FunctionalPrograms.java
+++ src/com/functionalprograms/FunctionalPrograms.java
@@ -4,7 +4,20 @@ import java.util.Scanner;
 
 public class FunctionalPrograms {
     public static void main(String[] args) {
+        int n[][] = new int[3][4];
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter 12 numbers");
 
-        System.out.println("Welcome to functional programs");
+        for (int i = 0; i < 3; i++) {
+            for(int j =0; j<4; j++){
+                n[i][j] = sc.nextInt();
+            }
+        }
+        for (int i =0; i<3; i++){
+            for (int j=0; j<4; j++){
+                System.out.print("["+i+"]["+j+"] " + n[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
