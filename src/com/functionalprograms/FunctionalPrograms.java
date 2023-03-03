1156d99ad9d803eb89802c2783e8532a4904d370
rahul12-99/rahul12-99 <rahulkrrao162@gmail.com>
2023-03-03 06:12:44 +0530

    Added: Code for finding quadratic equation roots

--- src/com/functionalprograms/FunctionalPrograms.java
+++ src/com/functionalprograms/FunctionalPrograms.java
@@ -3,18 +3,24 @@ package com.functionalprograms;
 import java.util.Scanner;
 
 public class FunctionalPrograms {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("enter x value ");
-        int x = sc.nextInt();
-        System.out.println("enter y value ");
-        int y = sc.nextInt();
-        distance(x, y);
+    public static void main(String[] Strings) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the value of a: ");
+        double a = scanner.nextDouble();
+        System.out.println("Enter the value of b: ");
+        double b = scanner.nextDouble();
+        System.out.println("Enter the value of c: ");
+        double c = scanner.nextDouble();
+        double discriminant = b * b - 4.0 * a * c;
+        if (discriminant > 0.0) {
+            double r1 = (-b + Math.pow(discriminant, 0.5)) / (2.0 * a);
+            double r2 = (-b - Math.pow(discriminant, 0.5)) / (2.0 * a);
+            System.out.println("The roots are " + r1 + " and " + r2);
+        } else if (discriminant == 0.0) {
+            double r1 = -b / (2.0 * a);
+            System.out.println("The root is " + r1);
+        } else {
+            System.out.println("Roots are not real.");
+        }
     }
-
-    public static void distance(int x, int y) {
-        double distance = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
-        System.out.println("Distance of " + x + ", " + y + " is : " + distance);
-    }
-
 }
