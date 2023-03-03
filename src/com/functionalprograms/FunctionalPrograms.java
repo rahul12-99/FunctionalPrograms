2485b8d295fae5281b51c09e2ebc5cfc11565fef
rahul12-99/rahul12-99 <rahulkrrao162@gmail.com>
2023-03-03 06:07:29 +0530

    Added: Code for find the euclidean distance

--- src/com/functionalprograms/FunctionalPrograms.java
+++ src/com/functionalprograms/FunctionalPrograms.java
@@ -1,28 +1,20 @@
 package com.functionalprograms;
 
+import java.util.Scanner;
+
 public class FunctionalPrograms {
-    static void findTriplets(int[] array, int length) {
-        boolean found = false;
-        for (int i = 0; i < length - 2; i++) {
-            for (int j = i + 1; j < length - 1; j++) {
-                for (int k = j + 1; k < length; k++) {
-                    if (array[i] + array[j] + array[k] == 0) {
-                        System.out.println(array[i] + " " + array[j] + " " + array[k]);
-                        found = true;
-                    }
-                }
-            }
-        }
-        if (found == false)
-            System.out.println(" Triplet not exist ");
-        else {
-            System.out.println("Triplet found");
-        }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter x value ");
+        int x = sc.nextInt();
+        System.out.println("enter y value ");
+        int y = sc.nextInt();
+        distance(x, y);
     }
 
-    public static void main(String[] args) {
-        int arr[] = {0,-1, 2, -3, 1};
-        int length = arr.length;
-        findTriplets(arr, length);
+    public static void distance(int x, int y) {
+        double distance = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+        System.out.println("Distance of " + x + ", " + y + " is : " + distance);
     }
+
 }
